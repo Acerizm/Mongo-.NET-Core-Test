42fcdf0e0b2ad35e625c688769042fa0ea89638d
Acerizm <34855234+Acerizm@users.noreply.github.com>
2019-01-03 00:20:55 +0800

    Update 1

---
+++ MongoDbAPI/MongoDbAPI/Models/IGameContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDbAPI.Models
+{
+    public interface IGameContext
+    {
+        IMongoCollection<Game> Games { get; }
+    }
+}
